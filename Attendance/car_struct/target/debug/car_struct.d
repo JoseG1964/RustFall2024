6887d6226a1d6d14321a3ebbb76d505aab7440f8
JoseG1964 <122923240+JoseG1964@users.noreply.github.com>
2024-09-30 17:58:41 +0000

    Attendance I/O

---
+++ Attendance/car_struct/target/debug/car_struct.d
@@ -0,0 +1,1 @@
+/workspaces/RustFall2024/Attendance/car_struct/target/debug/car_struct: /workspaces/RustFall2024/Attendance/car_struct/src/main.rs
---
+++ Attendance/car_struct/target/debug/deps/car_struct-bc81d3a9b355acc6.d
@@ -0,0 +1,5 @@
+/workspaces/RustFall2024/Attendance/car_struct/target/debug/deps/car_struct-bc81d3a9b355acc6: src/main.rs
+
+/workspaces/RustFall2024/Attendance/car_struct/target/debug/deps/car_struct-bc81d3a9b355acc6.d: src/main.rs
+
+src/main.rs:
